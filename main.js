faaa2dee8cf768939392cd02527b2a788a2c4814
Ramazanovakrom0723 <akromramazanov77@gmail.com>
2024-03-23 06:26:16 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,100 @@
+// 1-masala
+
+// let arr = [2,3,8,4,9,5,14]
+// let juft = 0
+// let toq = 0
+//     for(let i = 1; i <= arr.length; i++){
+//         if(arr[i] % 2 ===0){
+//             juft += 1
+//         }else{
+//             toq += 1
+//         }
+//     }
+// console.log(`${juft} ta juft son`);
+// console.log(`${juft} ta toq son`);
+
+
+
+
+
+// 2-masala 
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let juft = 0
+// let toq = 0
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 === 0){
+//         juft += arr[i]
+//     }else{
+//         toq += arr[i]
+//     }
+// }
+// console.log(`${juft-toq} ga teng juft va toq sonlar yig'indisi farqi`);
+
+
+
+
+
+// 3-masala
+
+// function count_arr(...params) {
+//     let argement = 0
+//     for(let i of params){
+//         argement += i
+//     }
+//     console.log(argement);
+// }
+// count_arr(1,9,3,5,7,13,17)
+
+
+
+
+
+// 4-masala
+
+// function String(arr) {
+//     let  Arry =[]
+//     for(element of arr){
+//         if ((typeof element)==='number') {
+//             Arry.push(element);
+//         }
+//     }
+//     console.log(Arry);
+// }
+// String([function name(params),"Juma",{name:"Akrom"},true])
+
+
+
+
+
+// 5- masala
+
+// let arr =[1,7,3,8]
+// function arry(params) {
+//     if(arr[0]>params){
+//         arr.unshift(params)
+       
+//     }else{
+//         arr.push(params)
+//     }
+//     console.log(arr);
+// }
+// arry(5)
+
+
+
+
+
+// 6- masala
+
+// function String(arr) {
+//   let arry=[]
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         arry.push(arr[i])
+//     }
+//   } 
+//   console.log(arry);
+// }
+// String([1,3,8,24,10,5,9,15,])
+
